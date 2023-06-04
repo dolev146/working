8f41f079f8dadfc80d34bf58f0fad546224dcd47
avgilboa <gilboa.aviad@gmail.com>
2023-06-04 16:20:39 +0300

    handshake functions

--- alice.py
+++ alice.py
@@ -1,22 +1,34 @@
 # alice.py
 import socket
 from Alicefunctions import Alice, generate_random_prime_with_n_bits
+
+def hand_shake_sever_bob(server_socket):
+    msg = server_socket.recv(1024).decode()
+    if  msg == "Ready":
+        server_socket.send(b"Acknowledged")
+        msg = server_socket.recv(1024).decode()
+        if msg == "Acknowledged":
+            return True
+    
+    return False
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_host = "0.0.0.0"  # listen on all available network interfaces
 server_port = 1234  # choose a port number
 server_socket.bind((server_host, server_port))
 server_socket.listen()
 print("Waiting for a connection...")
-client_socket, client_address = server_socket.accept()
+bob_socket, client_address = server_socket.accept()
 print("Connected to:", client_address)
+i = 0
 
 while True:
-    response = client_socket.recv(1024).decode()
-    if response == "Ready":
-        Alicebit = 0
+    if hand_shake_sever_bob(bob_socket):
+        Alicebit = 1
         q = generate_random_prime_with_n_bits(30)
         alice = Alice(Alicebit, q)
         i = i + 1
+        print(i)
         cA_q_g_gk = (
             str(alice.cA)
             + ","
@@ -26,20 +38,17 @@ while True:
             + ","
             + str(alice.gk)
         )
-        client_socket.send(b"Acknowledged")
-        response = client_socket.recv(1024).decode()
-        if response == "Acknowledged":
-            client_socket.send(cA_q_g_gk.encode())
-            cB = client_socket.recv(1024).decode()
-            cB_tuple = tuple(map(int, cB.strip("()").split(",")))
-            decrypted_result = alice.decrypt_message(cB_tuple)
+        bob_socket.send(cA_q_g_gk.encode())
+        cB = bob_socket.recv(1024).decode()
+        cB_tuple = tuple(map(int, cB.strip("()").split(",")))
+        decrypted_result = alice.decrypt_message(cB_tuple)
+        result = 0
+        if decrypted_result == 1:
             result = 0
-            if decrypted_result == 1:
-                result = 0
-            else:
-                result = 1
-            print(result)
-            client_socket.send(str(result).encode())
+        else:
+            result = 1
+        print(result)
+        bob_socket.send(str(result).encode())
 
-client_socket.close()
+bob_socket.close()
 server_socket.close()
--- bob.py
+++ bob.py
@@ -1,26 +1,33 @@
 # bob.py
 import socket
 from Bobfunctions import Bob
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def hand_shake_client_bob(server_socket):
+    server_socket.send(b"Ready")
+    Acknowledge = alice_server_socket.recv(1024).decode()
+    if Acknowledge == "Acknowledged":
+        server_socket.send(b"Acknowledged")
+        return True
+    return False
+    
+        
+alice_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_host = "localhost"  # Replace with the server's IP address
 server_port = 1234  # Replace with the server's port number
-client_socket.connect((server_host, server_port))
+alice_server_socket.connect((server_host, server_port))
 for i in range(1000):
-    client_socket.send(b"Ready")
-    response = client_socket.recv(1024).decode()
-    if response == "Acknowledged":
-        client_socket.send(b"Acknowledged")
+    if hand_shake_client_bob(alice_server_socket):
         Bobbit = 0
         bob = Bob(Bobbit)
-        response = client_socket.recv(1024).decode()
+        response = alice_server_socket.recv(1024).decode()
         cA_q_g_gk = response.split(",")
         cA = (int(cA_q_g_gk[0][1:]), int(cA_q_g_gk[1][:-1]))
         q = int(cA_q_g_gk[2])
         g = int(cA_q_g_gk[3])
         gk = int(cA_q_g_gk[4])
         bob.cB = bob.calc_encrypted_bit(cA, q, g, gk)
-        client_socket.send(str(bob.cB).encode())
-        result = client_socket.recv(1024).decode()
+        alice_server_socket.send(str(bob.cB).encode())
+        result = alice_server_socket.recv(1024).decode()
         print(result)
 
-client_socket.close()
+alice_server_socket.close()
